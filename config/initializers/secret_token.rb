a7f2e0320a8cf551677b64f9f6cf98b25b30f867
Jasdeep Jaitla <jasdeep@scalabl3.com>
2013-01-30 10:58:54 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twipster::Application.config.secret_token = 'f3d015f6640b52c5918994176d33d44fc862637caaea3a8ad2b22f86cfbeaa14e47a5595d2a835e9f1c036285b0471abe96e26c7aa4b55b6e3c64928963d922e'
